919c10141e2b410caccfb64d8ed114bbf5821c72
Sudha Rani <sudhatiwari22@gmail.com>
2025-06-01 23:16:10 +0530

    Unchecked optional for all attributes in Core Data Entity

--- Dex/Persistence.swift
+++ Dex/Persistence.swift
@@ -14,17 +14,24 @@ struct PersistenceController {
     static let preview: PersistenceController = {
         let result = PersistenceController(inMemory: true)
         let viewContext = result.container.viewContext
-        for _ in 0..<10 {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-        }
+        
+        let newPokemon = Pokemon(context: viewContext)
+        newPokemon.id = 1
+        newPokemon.name = "bulbasaur"
+        newPokemon.types = ["grass", "poison"]
+        newPokemon.hp = 45
+        newPokemon.attack = 49
+        newPokemon.defense = 49
+        newPokemon.specialAttach = 65
+        newPokemon.specialDefense = 65
+        newPokemon.speed = 45
+        newPokemon.sprite = URL(string: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png")
+        newPokemon.shiny = URL(string: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/1.png")
+        
         do {
             try viewContext.save()
         } catch {
-            // Replace this implementation with code to handle the error appropriately.
-            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-            let nsError = error as NSError
-            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            print(error)
         }
         return result
     }()
