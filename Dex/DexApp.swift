a7427157e9a5d4e1e96eaab56d76d802f5919494
Sudha Rani <sudhatiwari22@gmail.com>
2025-06-01 22:27:34 +0530

    Initial Commit

---
+++ Dex/DexApp.swift
@@ -0,0 +1,20 @@
+//
+//  DexApp.swift
+//  Dex
+//
+//  Created by Sudha Rani on 01/06/25.
+//
+
+import SwiftUI
+
+@main
+struct DexApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
