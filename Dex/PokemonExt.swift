625b19ce41718a42db606d4be8405bc9839e5d3e
Sudha Rani <sudhatiwari22@gmail.com>
2025-06-07 14:52:28 +0530

    Extend Pokemon Build stat Model and properties

---
+++ Dex/PokemonExt.swift
@@ -0,0 +1,55 @@
+//
+//  PokemonExt.swift
+//  Dex
+//
+//  Created by Sudha Rani on 07/06/25.
+//
+
+import SwiftUI
+
+extension Pokemon {
+    
+    var background: ImageResource {
+        switch types![0] {
+        case "rock", "ground", "steel", "fighting", "ghost", "dark", "psychic":
+                .rockgroundsteelfightingghostdarkpsychic
+        case "fire", "dragon":
+                .firedragon
+        case "flying", "bug":
+                .flyingbug
+        case "ice":
+                .ice
+        case "water":
+                .water
+        default:
+                .normalgrasselectricpoisonfairy
+        }
+    }
+    
+    var typeColor: Color {
+        Color(types![0].capitalized)
+    }
+    
+    var stats: [Stat] {
+        [
+            Stat(id: 1, name: "HP", value: hp),
+            Stat(id: 2, name: "Attack", value: attack),
+            Stat(id: 3, name: "Defense", value: defense),
+            Stat(id: 4, name: "Special Attack", value: specialAttack),
+            Stat(id: 5, name: "Special Defense", value: specialDefense),
+            Stat(id: 6, name: "Speed", value: speed),
+        ]
+    }
+    
+    var highestStat: Stat {
+        stats.max { $0.value < $1.value }!
+    }
+    
+}
+
+
+struct Stat: Identifiable {
+    var id: Int
+    var name: String
+    var value: Int16
+}
