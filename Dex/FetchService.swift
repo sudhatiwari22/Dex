c9cc9b1e7e200aea6fd5473e7b789752791632bb
Sudha Rani <sudhatiwari22@gmail.com>
2025-06-08 12:50:56 +0530

    Convert core date model to swiftData model

--- Dex/FetchService.swift
+++ Dex/FetchService.swift
@@ -15,7 +15,7 @@ struct FetchService {
     
     private let baseURL = URL(string: "https://pokeapi.co/api/v2/pokemon")!
     
-    func fetchPokemon(_ id: Int) async throws -> FetchedPokemon {
+    func fetchPokemon(_ id: Int) async throws -> Pokemon {
         let fetchURL = baseURL.appending(path: String(id))
         
         let (data, response) = try await URLSession.shared.data(from: fetchURL)
@@ -27,7 +27,7 @@ struct FetchService {
         let decoder = JSONDecoder()
         decoder.keyDecodingStrategy = .convertFromSnakeCase
         
-        let pokemon = try decoder.decode(FetchedPokemon.self, from: data)
+        let pokemon = try decoder.decode(Pokemon.self, from: data)
         
         print("Fetched Pokemon.. \(pokemon.id), \(pokemon.name.capitalized)")
         
--- Dex/Pokemon.swift
+++ Dex/Pokemon.swift
@@ -1,25 +1,33 @@
 //
-//  FetchedPokemon.swift
+//  Pokemon.swift
 //  Dex
 //
-//  Created by Sudha Rani on 01/06/25.
+//  Created by Sudha Rani on 08/06/25.
+//
 //
 
 import Foundation
+import SwiftData
+import SwiftUI
 
-struct FetchedPokemon: Decodable {
-    let id: Int16
-    let name: String
-    let types: [String]
-    let hp: Int16
-    let attack: Int16
-    let defense: Int16
-    let specialAttack: Int16
-    let specialDefense: Int16
-    let speed: Int16
-    let spriteURL: URL
-    let shinyURL: URL
+@Model
+class Pokemon: Decodable {
     
+    @Attribute(.unique) var id: Int
+    var name: String
+    var types: [String]
+    var hp: Int
+    var attack: Int
+    var defense: Int
+    var specialAttack: Int
+    var specialDefense: Int
+    var speed: Int
+    var spriteURL: URL
+    var shinyURL: URL
+    var shiny: Data?
+    var sprite: Data?
+    var favorite: Bool = false
+
     enum CodingKeys: CodingKey {
         case id
         case name
@@ -44,11 +52,11 @@ struct FetchedPokemon: Decodable {
             case shinyURL = "frontShiny"
         }
     }
-    
-    init(from decoder: any Decoder) throws {
+
+    required init(from decoder: any Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         
-        self.id = try container.decode(Int16.self, forKey: .id)
+        self.id = try container.decode(Int.self, forKey: .id)
         self.name = try container.decode(String.self, forKey: .name)
         
         var decodeTypes: [String] = []
@@ -71,12 +79,12 @@ struct FetchedPokemon: Decodable {
         self.types = decodeTypes
         
         
-        var decodeStats: [Int16] = []
+        var decodeStats: [Int] = []
         var statsContainer = try container.nestedUnkeyedContainer(forKey: .stats)
         while !statsContainer.isAtEnd {
             let statsDictionaryContainer = try statsContainer.nestedContainer(keyedBy: CodingKeys.StatsDictionaryKeys.self)
             
-            let stat = try statsDictionaryContainer.decode(Int16.self, forKey: .baseStat)
+            let stat = try statsDictionaryContainer.decode(Int.self, forKey: .baseStat)
             decodeStats.append(stat)
         }
         
@@ -91,4 +99,64 @@ struct FetchedPokemon: Decodable {
         self.spriteURL = try spriteContainer.decode(URL.self, forKey: .spriteURL)
         self.shinyURL = try spriteContainer.decode(URL.self, forKey: .shinyURL)
     }
+    
+    
+    var spriteImage: Image {
+        if let data = sprite, let image = UIImage(data: data) {
+            Image(uiImage: image)
+        } else {
+            Image(.bulbasaur)
+        }
+    }
+    
+    var shinyImage: Image {
+        if let data = shiny, let image = UIImage(data: data) {
+            Image(uiImage: image)
+        } else {
+            Image(.bulbasaur)
+        }
+    }
+    
+    var background: ImageResource {
+        switch types[0] {
+        case "rock", "ground", "steel", "fighting", "ghost", "dark", "psychic":
+                .rockgroundsteelfightingghostdarkpsychic
+        case "fire", "dragon":
+                .firedragon
+        case "flying", "bug":
+                .flyingbug
+        case "ice":
+                .ice
+        case "water":
+                .water
+        default:
+                .normalgrasselectricpoisonfairy
+        }
+    }
+    
+    var typeColor: Color {
+        Color(types[0].capitalized)
+    }
+    
+    var stats: [Stat] {
+        [
+            Stat(id: 1, name: "HP", value: hp),
+            Stat(id: 2, name: "Attack", value: attack),
+            Stat(id: 3, name: "Defense", value: defense),
+            Stat(id: 4, name: "Special Attack", value: specialAttack),
+            Stat(id: 5, name: "Special Defense", value: specialDefense),
+            Stat(id: 6, name: "Speed", value: speed),
+        ]
+    }
+    
+    var highestStat: Stat {
+        stats.max { $0.value < $1.value }!
+    }
+    
+    struct Stat: Identifiable {
+        var id: Int
+        var name: String
+        var value: Int
+    }
+
 }
